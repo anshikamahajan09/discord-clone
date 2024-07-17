9e3bffa8cbae7a3ce0f68ef4ad88b17e02238dcb
anshikamahajan09 <anshika1304.be22@chitkara.edu.in>
2024-07-17 23:33:26 +0530

    invite modal created

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -1,4 +1,4 @@
-"use-client";
+"use client";
 
 import { ServerWithMembersWithProfiles } from "@/types";
 import { MemberRole } from "@prisma/client";
@@ -11,6 +11,7 @@ import {
     DropdownMenuTrigger,
   } from "@/components/ui/dropdown-menu"
 import { ChevronDown, LogOut, PlusCircle, Settings, Trash, UserPlus, Users } from "lucide-react";
+import { useModal } from "@/hooks/use-modal-store";
   
 
 interface ServerHeaderProps {
@@ -19,19 +20,22 @@ interface ServerHeaderProps {
 }
 
 export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
+
+  const {onOpen} = useModal();
+
   const isAdmin = role === MemberRole.ADMIN;
   const isModerator = isAdmin || role === MemberRole.MODERATOR;
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="focus:outline-none" asChild>
-        <button className="w-full text-md font-semibold px-3 flex items-center h-12 bordee-neutral-200 dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10 dark:hober:bg-zinc-700/50 transition">
+        <button className="w-full text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition">
             {server.name}
             <ChevronDown className="ml-auto h-5 w-5"/>
         </button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 spcae-y-[2px]">
+      <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]">
         {isModerator && (
-            <DropdownMenuItem className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
+            <DropdownMenuItem onClick={()=>onOpen("invite",{server})} className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
                 Invite People
             <UserPlus className="ml-auto h-4 w-4"/>
             </DropdownMenuItem>
